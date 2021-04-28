9f5fdea23527f25c1d837dd17ef0f3bfbaa362c5
Christopher Sund <csunday95@gmail.com>
2021-04-27 19:10:10 -0700

    some basic framework code

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <coreinit/thread.h>
+#include <coreinit/time.h>
+#include <coreinit/systeminfo.h>
+#include <nn/ac.h>
+
+#include <whb/proc.h>
+#include <whb/log.h>
+#include <whb/log_console.h>
+
+#include <thread>
+
+#include "utility/byteswap.hpp"
+
+int
+hello_thread()
+{
+   int last_tm_sec = -1;
+   uint32_t ip = 0;
+   WHBLogPrintf("Hello World from a std::thread!");
+
+   if (!nn::ac::GetAssignedAddress(&ip)) {
+      WHBLogPrintf("GetAssignedAddress failed!");
+   }
+
+   WHBLogPrintf("My IP is: %u.%u.%u.%u",
+                (ip >> 24) & 0xFF,
+                (ip >> 16) & 0xFF,
+                (ip >>  8) & 0xFF,
+                (ip >>  0) & 0xFF);
+
+   Utility::byteswap(ip);
+   uint16_t s = 0xABCD;
+   Utility::byteswap(s);
+
+   while(WHBProcIsRunning()) {
+      OSCalendarTime tm;
+      OSTicksToCalendarTime(OSGetTime(), &tm);
+
+      if (tm.tm_sec != last_tm_sec) {
+         WHBLogPrintf("%02d/%02d/%04d %02d:%02d:%02d I'm still here.",
+                      tm.tm_mday, tm.tm_mon, tm.tm_year,
+                      tm.tm_hour, tm.tm_min, tm.tm_sec);
+         last_tm_sec = tm.tm_sec;
+      }
+
+      WHBLogConsoleDraw();
+      OSSleepTicks(OSMillisecondsToTicks(100));
+   }
+
+   WHBLogPrintf("Exiting... good bye.");
+   WHBLogConsoleDraw();
+   OSSleepTicks(OSMillisecondsToTicks(1000));
+   return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+   nn::ac::ConfigIdNum configId;
+
+   nn::ac::Initialize();
+   nn::ac::GetStartupId(&configId);
+   nn::ac::Connect(configId);
+
+   WHBProcInit();
+   WHBLogConsoleInit();
+
+   std::thread t(hello_thread);
+   t.join();
+
+   WHBLogConsoleFree();
+   WHBProcShutdown();
+
+   nn::ac::Finalize();
+   return 0;
+}
